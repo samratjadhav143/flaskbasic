5d614f7729a3211d1244c2570d4c3ac41448e351
samratjadhav143 <138105982+samratjadhav143@users.noreply.github.com>
2023-08-01 08:56:22 +0530

    the render template code

--- flaskCode.py
+++ flaskCode.py
@@ -1,19 +1,57 @@
-from flask import Flask
-from flask import request
+from flask import Flask,request ,render_template , jsonify
+
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_dear():
-    return "<h1>I LOVE YOU DARLING ..!</h1>"
+def home_page():
+    return render_template('index.html')
 
-@app.route('/hello_feelings')
-def hello_feelings():
-    return "<h1>I CAN'T LIVE WITH OUT YOU ..!</h1>"
 
-@app.route('/hello_feelings_1')
-def hello_feelings_1():
-    data = request.args.get('x')
-    return "THIS IS MY NAME {}".format(data)
+@app.route('/math',methods=['POST'])
+def math_ops():
+    if(request.method == 'POST'):
+        ops = request.form['operation']
+        num1 = int(request.form['num1'])
+        num2 = int(request.form['num2'])
+        if ops == 'add':
+            r = num1+num2
+            result = "The sum of " + str(num1) + 'and ' + str(num2) + "is " + str(r)
+        if ops == 'subtract':
+            r = num1-num2
+            result = "The subtract of " + str(num1) + 'and ' + str(num2) + "is " + str(r)
+        if ops == 'multiply':
+            r = num1*num2
+            result = "The multiply of " + str(num1) + 'and ' + str(num2) + "is " + str(r)
+        if ops == 'divide':
+            r = num1/num2
+            result = "The divide of " + str(num1) + 'and ' + str(num2) + "is " + str(r)
+            
+        return render_template('results.html' , result = result)
+
+
+
+
+@app.route('/postman_action',methods=['POST'])
+def math_ops1():
+    if(request.method == 'POST'):
+        ops = request.json['operation']
+        num1 = int(request.json['num1'])
+        num2 = int(request.json['num2'])
+        if ops == 'add':
+            r = num1+num2
+            result = "The sum of " + str(num1) + 'and ' + str(num2) + "is " + str(r)
+        if ops == 'subtract':
+            r = num1-num2
+            result = "The subtract of " + str(num1) + 'and ' + str(num2) + "is " + str(r)
+        if ops == 'multiply':
+            r = num1*num2
+            result = "The multiply of " + str(num1) + 'and ' + str(num2) + "is " + str(r)
+        if ops == 'divide':
+            r = num1/num2
+            result = "The divide of " + str(num1) + 'and ' + str(num2) + "is " + str(r)
+            
+        return jsonify(result)
 
 if __name__=="__main__":
     app.run(host="0.0.0.0")
